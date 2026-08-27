ca3bda0a5fb671460791e01656b4006909a4a336
jpdear <jdearing@byu.edu>
2026-08-27 12:15:29 -0600

    check checks that the file exists

--- src/lcc/cli.py
+++ src/lcc/cli.py
@@ -1,9 +1,27 @@
+from pathlib import Path
+
 import click
 
 
 @click.group()
 def app():
     pass
+
+
+@app.command()
+@click.pass_context
+@click.argument("source", type=click.File("r"), default="-")
+@click.option(
+    "--url",
+    "urls",
+    multiple=True,
+    help="Check SOURCE against these URLs instead of any found in SOURCE.",
+)
+def check(ctx, source, urls):
+    file_path = Path(source)
+
+    if not file_path.is_file():
+        ctx.exit(1)
 
 
 if __name__ == "__main__":
